13f8a50980a3c52eeb56ddc493202a62c36e38b4
Owen <jiawei.me@hotmail.com>
2023-07-11 11:41:28 +0800

    init

---
+++ src/main/java/com/hanjiawei/codeploy/CodeployApplication.java
@@ -0,0 +1,13 @@
+package com.hanjiawei.codeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanjiawei/codeploy/CodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanjiawei.codeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
