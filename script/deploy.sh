9b0f06ea8328c789d9a23ff5a5188d3e1bc2fa6c
Owen <jiawei.me@hotmail.com>
2023-07-21 16:08:09 +0800

    完善一下

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@
 giturl=$1
 branch=$2
 buildcmd=$3
-
+tag=$4
 cd /root/deployworkspace
 
 # 获取项目目录名称
@@ -15,4 +15,4 @@ git clone $giturl -b $branch
 cd $repo_name
 
 # 执行构建命令
-$buildcmd
+docker build -t repo_name:$tag -f Dockerfile .
--- src/main/java/com/hanjiawei/codeploy/service/NginxDeployService.java
+++ src/main/java/com/hanjiawei/codeploy/service/NginxDeployService.java
@@ -4,11 +4,24 @@ import cn.hutool.extra.ssh.JschUtil;
 import com.jcraft.jsch.Session;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class NginxDeployService {
 
 
-    public void deploy(){
+    public void deploy() {
+        String projectName = "codeploy";
+        String gitUrl = "";
+        String branch = "main";
+        String port = "8080";
+        String containerPort = "8080";
+        // tag格式 当前日期+时间戳
+        String tag = LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+//        String dockerImageName = String.format("%s:%s", projectName, tag);
+        String dockerBuildCmd = String.format("docker build -t %s:%s -f ./Dockerfile .", projectName, tag);
+
+        String dockerRunCmd = String.format("docker run -d -p %s:%s --name %s %s:%s", port, containerPort, projectName, projectName, tag);
+
         Session session = JschUtil.getSession("server.banip.top", 22, "root", "123");
         // 由项目自己配置
         String packageCmd = "npm ";
