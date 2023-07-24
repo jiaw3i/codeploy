679d22f57c2c458f8a2586137f6f80e3af80936f
Owen <jiawei.me@hotmail.com>
2023-07-24 22:58:36 +0800

    配置文件

--- src/main/java/com/hanjiawei/codeploy/CodeployApplication.java
+++ src/main/java/com/hanjiawei/codeploy/CodeployApplication.java
@@ -3,9 +3,11 @@ package com.hanjiawei.codeploy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class CodeployApplication {
 
 	public static void main(String[] args) {
